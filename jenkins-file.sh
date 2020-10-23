61e5d548feac1040c7522a818702c85092cf5f9b
lisandrodv <lisandrodv27@gmail.com>
2020-10-23 12:54:40 +0000

    third commit. Added version output

--- jenkins-file.sh
+++ jenkins-file.sh
@@ -4,4 +4,6 @@ ls -la
 echo "Thank you. Now please print the current working directory"
 echo "Yes, of course"
 pwd
-
+echo "Please print out the current project version"
+echo ${VERSION}
+echo "Thank you"
